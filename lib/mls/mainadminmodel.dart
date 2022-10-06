87723aad166b1687709e6b1e3f74be61f7593d9c
Sifat-16 <59054820+Sifat-16@users.noreply.github.com>
2022-10-06 10:05:15 +0600

    Add files via upload

--- lib/mls/mainadminmodel.dart
+++ lib/mls/mainadminmodel.dart
@@ -1,9 +1,13 @@
 class MainAdminModel{
   String? bkash;
   String? nagad;
+  String? rocket;
+  String? upay;
 
   MainAdminModel.fromJson(Map<String, dynamic>json){
     bkash = json["bkash"];
     nagad = json["nagad"];
+    rocket = json["rocket"];
+    upay = json["upay"];
   }
 }
